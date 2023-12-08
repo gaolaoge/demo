5ed55a037eee50ed8d99ea52d785051b371e368e
gaolaoge <gaolaoge@sina.cn>
2023-12-09 02:15:41 +0800

    chore: 添加配置项

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "@babel/eslint-parser", // 解析器
+    sourceType: "module",
+    ecmaVersion: 12
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    "plugin:vue/vue3-recommended", // plugin-vue
+    "eslint:recommended", // eslint
+    "plugin:prettier/recommended" // plugin-prettier
+  ],
+
+  rules: {
+    "prettier/prettier": "error"
+  },
+  workingDirectories: [
+    {
+      mode: "auto"
+    }
+  ]
+};
---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "revert", "build"]
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-empty": [2, "always"],
+    "scope-case": [0],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [0, "always", 72]
+  }
+};
