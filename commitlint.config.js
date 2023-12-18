4315ddbdea3c16ad1530786d0c4a09802d6e5d85
gaoge <gaolaoge@sina.cn>
2023-12-18 12:27:16 +0800

    chore: 更新维护逻辑

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "revert", "build"]
+      ["demo", "feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "revert", "build"]
     ],
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
